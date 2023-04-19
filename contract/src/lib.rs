c604b69c7452b2396531828591645516e880791d
advanced-user <stolbetskiy.yura@gmail.com>
2023-04-19 04:03:37 +0300

    Implemented get_random_room

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,7 +1,7 @@
 use crate::KeyStore::{AppRooms, Rooms, RoomsPerApp, RoomsPerAppOwner, RoomsPerOwner};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::UnorderedMap;
-use near_sdk::env::predecessor_account_id;
+use near_sdk::env::{predecessor_account_id, random_seed};
 use near_sdk::json_types::U128;
 use near_sdk::serde::Serialize;
 use near_sdk::store::{LookupMap, UnorderedSet};
@@ -84,6 +84,29 @@ impl Contract {
             .take(limit.unwrap_or(0))
             .map(|x| self.rooms.get(x).expect("Room not found").clone())
             .collect()
+    }
+
+    pub fn get_random_room(&self, app_name: AppName) -> Room {
+        let app_rooms = self
+            .rooms_per_app
+            .get(&app_name)
+            .expect("App rooms not found");
+
+        let room_ids: Vec<&RoomId> = app_rooms.iter().collect();
+        let number_of_rooms = room_ids.len() as usize;
+        let rnd_idx = self.get_random_in_range(0, number_of_rooms, 0);
+        let rnd_room_id = room_ids.get(rnd_idx).expect("Random room id not found");
+
+        self.rooms
+            .get(rnd_room_id)
+            .expect("Random room not found")
+            .clone()
+    }
+
+    pub fn get_random_in_range(&self, min: usize, max: usize, index: usize) -> usize {
+        let random = *random_seed().get(index).unwrap();
+        let random_in_range = (random as f64 / 256.0) * (max - min) as f64 + min as f64;
+        random_in_range.floor() as usize
     }
 
     pub fn create_room(&mut self, room_config: RoomConfig) -> RoomId {
